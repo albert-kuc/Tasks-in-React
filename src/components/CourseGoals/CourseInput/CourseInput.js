87fba3f98cbbfce2eb82de1b7e392787ebbaec84
Albert Kuc <albert.kuc@mssint.com>
2022-04-01 14:07:00 +0100

    apply styled-components to CourseInput

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,38 @@
 import React, { useState } from 'react';
+import styled from "styled-components";
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+
+/**
+ * Div styled component to replace regular div with css
+ * @type {StyledComponent<"div", AnyIfEmpty<DefaultTheme>, {}, never>}
+ */
+const FormControl = styled.div`
+    margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${props => (props.invalid ? 'red' : 'black')};
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => (props.invalid ? 'crimson' : '#ccc')};
+    background: ${props => (props.invalid ? '#fad0ec' : 'transparent')};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #b7f8b7;
+    border-color: #8b005d;
+  }
+`;
 
 const CourseInput = props => {
   /**
@@ -44,10 +75,10 @@ const CourseInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       {/* Dynamically update input and label to either `form-control` or `form-control invalid` */}
-      <div className={`form-control ${!isValid ? 'invalid': ''}`}>
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
